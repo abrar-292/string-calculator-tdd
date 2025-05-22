466c0a2da6f8a102bc48a4b4a7f14b9aae0bb8c8
Abrar Sanandwala <139760904+AbrarSwala@users.noreply.github.com>
2025-05-22 16:10:32 +0530

    Support custom delimiters in input

--- calculator.js
+++ calculator.js
@@ -1,9 +1,15 @@
 function add(numbers) {
   if (numbers === "") return 0;
 
-  const sanitized = numbers.replace(/\n/g, ",");
-  return sanitized
-    .split(",")
+  let delimiter = /,|\n/;
+  if (numbers.startsWith("//")) {
+    const match = numbers.match(/^\/\/(.+)\n(.*)$/);
+    delimiter = new RegExp(match[1]);
+    numbers = match[2];
+  }
+
+  return numbers
+    .split(delimiter)
     .map((n) => parseInt(n))
     .reduce((sum, n) => sum + n, 0);
 }
--- calculator.test.js
+++ calculator.test.js
@@ -15,3 +15,7 @@ test("returns sum for multiple comma-separated numbers", () => {
 test("handles new lines between numbers", () => {
   expect(add("1\n2,3")).toBe(6);
 });
+
+test("supports custom delimiters", () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
