c7aaa93b883c07bdfc9a03cacf963ca92fcf611e
Abrar Sanandwala <139760904+AbrarSwala@users.noreply.github.com>
2025-05-22 16:05:57 +0530

    Handle two comma-separated numbers

--- calculator.js
+++ calculator.js
@@ -1,5 +1,8 @@
 function add(numbers) {
   if (numbers === "") return 0;
-  return parseInt(numbers);
+
+  const parts = numbers.split(",");
+  const nums = parts.map((n) => parseInt(n));
+  return nums.reduce((a, b) => a + b, 0);
 }
 module.exports = { add };
